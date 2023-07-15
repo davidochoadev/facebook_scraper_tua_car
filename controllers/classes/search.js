6069d26be07173c58654f9130feeb0e4985d53e2
davideochoaa <davideochoaa@gmail.com>
2023-07-15 17:15:55 +0200

    🛠 Fix: Fixing login on vps 11

--- controllers/classes/search.js
+++ controllers/classes/search.js
@@ -16,8 +16,8 @@ export default class Search{
     
     async main (location){
         console.log(chalk.yellow("Starting Puppeteer..."))
-        try{
-        const browser = await puppeteer.launch({ headless: !0 });
+    try{
+        const browser = await puppeteer.launch({ headless: true });
         this.page = await browser.newPage();
         await browser.close();
         return {successfull: "Successfull Login!"}
